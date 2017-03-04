907706ce8f142abe3f816357ef8cbadeb117fa9a
Simon Staton <simon.staton@live.co.uk>
2017-03-04 13:46:03 +0000

    Setting up meetings feed for meetings list page

--- scripts/src/pages/meetingsList.js
+++ scripts/src/pages/meetingsList.js
@@ -1,15 +1,26 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import Api from '../utils/api';
 
 class Actions extends React.Component {
 
   constructor(props) {
     super(props);
     this.props = props;
-    this.state = {};
+    this.state = {
+      meetings: []
+    };
+    this.getData();
+  }
+
+  async getData() {
+    this.setState({
+      meetings: (await Api.getMeetings()).data
+    });
   }
 
   render(){
+    console.log(this.state.meetings);
     return (
         <div></div>
     );
