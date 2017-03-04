8244f59d909302cb4bd00d91f44e1376900908b8
Simon Staton <simon.staton@live.co.uk>
2017-03-04 22:08:30 +0000

    Setting up user login

--- scripts/src/pages/login.js
+++ scripts/src/pages/login.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import view from './views/login';
+import api from '../utils/api';
 
 class Home extends React.Component {
 
@@ -9,10 +10,46 @@ class Home extends React.Component {
     this.state = {};
   }
 
+  validate(e){
+    e.preventDefault();
+
+    // Get inputs
+    var self = this,
+      valid = true,
+      inputs = Object.keys(this.refs).filter(
+        key => key.indexOf('form-field-') == 0
+      ).reduce((data, key) => {
+        data[key] = self.refs[key];
+        return data;
+      }, {});
+
+    // Validate all inputs
+    for(let key in inputs) if(inputs[key].validate && !inputs[key].validate()) valid = false;
+
+    // Handle validation
+    if(!valid) {
+      e.preventDefault();
+    } else {
+      const formData = Array.from(e.target.elements)
+        .filter(el => el.name)
+        .reduce((a, b) => ({...a, [b.name]: b.value}), {});
+      this.login(formData);
+    }
+  }
+
+  async login(data) {
+    api.getUsers().then(res => {
+      const user = res.data.filter(user =>  user.email == data.email)[0];
+      if (user) {
+        console.log("user found", user);
+      } else {
+        alert("invalid email");
+      }
+    });
+  }
+
   render(){
-    return (
-      <div />
-    );
+    return view(this);
   }
 
 };
--- scripts/src/pages/views/login.js
+++ scripts/src/pages/views/login.js
@@ -1,34 +1,40 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import FormFields from 'react-form-fields';
 
 export default (self) => {
     return (
         <div>
             <Helmet
-                title="Just Meet | Login"
+                title="Just Meet | Overview"
             />
 
-            <div className="hero">
-                <span className="logo">Just Meet</span>
+            <div className="left quarter">
+                <nav className="side-nav">
+                    <a href="#">Login</a>
+                    <a href="#">Register</a>
+                </nav>
             </div>
 
-            <div className="nav-bar">
-                <div className="container">
-                    <nav>
-                        <a href="#">Login</a>
-                        <a href="#">Register</a>
-                        <a href="#">Forgot Password</a>
-                    </nav>
-                </div>
+            <div className="right three-quarters white-bg padding-medium">
+                <h2>Login</h2>
+                <form onSubmit={self.validate.bind(self)}>
+                  <FormFields
+                    tag="input" 
+                    validation="email" 
+                    errorMsg="Email is required" 
+                    ref="form-field-1"  
+                    required={true} 
+                    attributes={{
+                      type: 'text',
+                      placeholder: 'Email Address',
+                      name: 'email',
+                      id: 'email'
+                    }}
+                  />
+                  <button>Login</button>
+                </form>
             </div>
-
-            <main>
-                <div className="container">
-                  <form>
-                    
-                  </form>
-                </div>
-            </main>
         </div>
     );
 };
