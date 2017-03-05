6d08e9effa6fa3c23b1ebbb6e8c49d520e022cf3
Tom Clarke <tomc@designyourcode.co.uk>
2017-03-05 08:10:56 +0000

    Adding pagination and search to table

--- scripts/src/pages/rooms.js
+++ scripts/src/pages/rooms.js
@@ -1,29 +1,31 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { Link } from 'react-router';
 import Api from '../utils/api';
+import view from './views/rooms';
+import menu from '../utils/menu';
 
 class Rooms extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.props = props;
-    this.state = {
-      rooms: []
-    };
-    this.getData();
-  }
+    constructor(props) {
+        super(props);
+        this.props = props;
+        this.state = {
+            menu: menu,
+            rooms: []
+        };
+        this.getData();
+    }
 
-  async getData() {
-    // this.setState({
-    //   rooms: (await Api.getRooms()).data
-    // });
-  }
+    async getData() {
+        // this.setState({
+        //   rooms: (await Api.getRooms()).data
+        // });
+    }
 
-  render(){
-    return (
-        <div>Rooms</div>
-    );
-  }
+    render(){
+        return view(this);
+    }
 };
 
 export default Rooms;
---
+++ scripts/src/pages/views/rooms.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Helmet from 'react-helmet'
+import { Link } from 'react-router';
+
+export default (self) => {
+    return (
+        <div>
+            <Helmet
+                title="Just Meet | Rooms"
+            />
+
+            <div className="left quarter">
+                <nav className="side-nav">
+                    {self.state.menu.map((link, index) => {
+                        return <Link key={index} to={link.path}><i className={'fa fa-' + link.icon} aria-hidden="true"></i> {link.text}</Link>
+                    })}
+                </nav>
+            </div>
+
+            <div className="right three-quarters white-bg padding-medium">
+                <h2>Rooms</h2>
+            </div>
+        </div>
+    );
+};
